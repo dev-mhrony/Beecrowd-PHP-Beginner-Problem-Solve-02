9862191a2b89ec05aa6a72c9303d4383431d5976
MH-RONY <mh.rony.dhanvi@gmail.com>
2022-06-10 12:39:50 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<?php
+
+
+/* Read 2 variables, named A and B and make the sum of these two variables, assigning its result to the variable X. Print X as shown below. Print endline after the result otherwise you will get “Presentation Error”.
+ Input
+ The input file will contain 2 integer numbers.
+ Output
+ Print the letter X (uppercase) with a blank space before and after the equal signal followed by the value of X, according to the following example.
+ Obs.: don't forget the endline after all.
+
+*/
+
+
+
+// Solve 
+
+/**
+ * Here a and b are the two input variables.
+ * By adding values to the variables a and b it is stored in a variable named x which is then printed by echo.
+ * The following 3 examples have been printed separately.
+ * The line break has been given through the br tag.
+ */
+
+// Example 01
+
+
+
+$a = 10;
+$b = 9;
+
+$x = $a + $b;
+
+echo "x = $x";
+echo "<br>";
+
+// ==================================================
+
+
+// Example 02
+
+$a = -10;
+$b = 4;
+
+$x = $a + $b;
+
+echo "x = $x";
+
+echo "<br>";
+
+// ==================================================
+
+
+// Example 03
+
+$a = 15;
+$b = -7;
+
+$x = $a + $b;
+
+echo "x = $x";
+
+
+// Many thanks for verifying my solution.
+
+?>
